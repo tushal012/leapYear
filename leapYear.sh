a21bfd8bd36415e1830b207ba863594cf504586f
tushal012 <tushalkashyap11@gmail.com>
2021-05-11 21:39:03 +0530

    particular year is leap year or not

---
+++ leapYear.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -x
+
+echo -n "Enter year (YYYY): "
+read y
+a = '$y%4'
+b = '$y%100'
+c = '$y%400'
+if[$a -eq 0 -a $b -ne - -o $c -eq 0]
+then 
+echo "$y is leap year"
+else
+echo "$y is not a leap year"
+
+fi
